0af18c95e205cc6b7a907d40b5a261b3c1260ae9
thang31798 <thang1233214q@gmail.com>
2023-11-17 14:35:08 +0700

    project

---
+++ view/footer.php
@@ -0,0 +1,250 @@
+<div class="prs_newsletter_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+					<div class="prs_newsletter_text">
+						<h3>Get update sign up now !</h3>
+					</div>
+				</div>
+				<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+					<div class="prs_newsletter_field">
+						<input type="text" placeholder="Enter Your Email">
+						<button type="submit">Submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- prs Newsletter Wrapper End -->
+	<!-- prs footer Wrapper Start -->
+	<div class="prs_footer_main_section_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="prs_footer_cont1_wrapper prs_footer_cont1_wrapper_1">
+						<h2>LANGUAGE MOVIES</h2>
+						<ul>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">English movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Tamil movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Punjabi Movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Hindi movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Malyalam movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">English Action movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Hindi Action movie</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="prs_footer_cont1_wrapper prs_footer_cont1_wrapper_2">
+						<h2>MOVIES by presenter</h2>
+						<ul>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Action movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Romantic movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Adult movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Comedy movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Drama movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Musical movie</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">Classical movie</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="prs_footer_cont1_wrapper prs_footer_cont1_wrapper_3">
+						<h2>BOOKING ONLINE</h2>
+						<ul>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.example.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.hello.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.example.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.hello.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.example.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.hello.com</a>
+							</li>
+							<li><i class="fa fa-circle"></i> &nbsp;&nbsp;<a href="#">www.example.com</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="prs_footer_cont1_wrapper prs_footer_cont1_wrapper_4">
+						<h2>App available on</h2>
+						<p>Download App and Get Free Movie Ticket !</p>
+						<ul>
+							<li>
+								<a href="#">
+									<img src="images/content/f1.jpg" alt="footer_img">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/content/f2.jpg" alt="footer_img">
+								</a>
+							</li>
+						</ul>
+						<h5><span>$50</span> Payback on App Download</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="prs_bottom_footer_wrapper">	<a href="javascript:" id="return-to-top"><i class="flaticon-play-button"></i></a>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+					<div class="prs_bottom_footer_cont_wrapper">
+						<p>Copyright 2022-23 <a href="#">Movie Pro</a> . All rights reserved - Design by <a href="#">Webstrot</a>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-4 col-xs-12">
+					<div class="prs_footer_social_wrapper">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a>
+							</li>
+							<li><a href="#"><i class="fa fa-linkedin"></i></a>
+							</li>
+							<li><a href="#"><i class="fa fa-youtube-play"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- prs footer Wrapper End -->
+	<!-- st login wrapper Start -->
+	<div class="modal fade st_pop_form_wrapper" id="myModal" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<div class="st_pop_form_heading_wrapper float_left">
+					<h3>Log in</h3>
+				</div>
+				<div class="st_profile_input float_left">
+					<label>Email / Mobile Number</label>
+					<input type="text">
+				</div>
+				<div class="st_profile__pass_input st_profile__pass_input_pop float_left">
+					<input type="password" placeholder="Password">
+				</div>
+				<div class="st_form_pop_fp float_left">
+					<h3><a href="#" data-toggle="modal" data-target="#myModa2" target="_blank">Forgot Password?</a></h3>
+				</div>
+				<div class="st_form_pop_login_btn float_left">	<a href="https://webstrot.com/html/moviepro/html/page-1-7_profile_settings.html">LOGIN</a>
+				</div>
+				<div class="st_form_pop_or_btn float_left">
+					<h4>or</h4>
+				</div>
+				<div class="st_form_pop_facebook_btn float_left">	<a href="#"> Connect with Facebook</a>
+				</div>
+				<div class="st_form_pop_gmail_btn float_left">	<a href="#"> Connect with Google</a>
+				</div>
+				<div class="st_form_pop_signin_btn float_left">
+					<h4>Don’t have an account? <a href="#" data-toggle="modal" data-target="#myModa3" target="_blank">Sign Up</a></h4>
+					<h5>I agree to the <a href="#">Terms & Conditions</a> & <a href="#">Privacy Policy</a></h5>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade st_pop_form_wrapper" id="myModa2" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<div class="st_pop_form_heading_wrapper st_pop_form_heading_wrapper_fpass float_left">
+					<h3>Forgot Password</h3>
+					<p>We can help! All you need to do is enter your email ID and follow the instructions!</p>
+				</div>
+				<div class="st_profile_input float_left">
+					<label>Email Address</label>
+					<input type="text">
+				</div>
+				<div class="st_form_pop_fpass_btn float_left">	<a href="#">Verify</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade st_pop_form_wrapper" id="myModa3" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<div class="st_pop_form_heading_wrapper float_left">
+					<h3>Sign Up</h3>
+				</div>
+				<div class="st_profile_input float_left">
+					<label>Email / Mobile Number</label>
+					<input type="text">
+				</div>
+				<div class="st_profile__pass_input st_profile__pass_input_pop float_left">
+					<input type="password" placeholder="Password">
+				</div>
+				<div class="st_form_pop_fp float_left">
+					<h3><a href="#" data-toggle="modal" data-target="#myModa2" target="_blank">Forgot Password?</a></h3>
+				</div>
+				<div class="st_form_pop_login_btn float_left">	<a href="https://webstrot.com/html/moviepro/html/page-1-7_profile_settings.html">LOGIN</a>
+				</div>
+				<div class="st_form_pop_or_btn float_left">
+					<h4>or</h4>
+				</div>
+				<div class="st_form_pop_facebook_btn float_left">	<a href="#"><i class="fab fa-facebook-f"></i> Connect with Facebook</a>
+				</div>
+				<div class="st_form_pop_gmail_btn float_left">	<a href="#"><i class="fab fa-google-plus-g"></i> Connect with Google</a>
+				</div>
+				<div class="st_form_pop_signin_btn st_form_pop_signin_btn_signup float_left">
+					<h5>I agree to the <a href="#">Terms & Conditions</a> & <a href="#">Privacy Policy</a></h5>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- st login wrapper End -->
+	<!--main js file start-->
+	<script src="../js/jquery_min.js"></script>
+	<script src="../js/modernizr.js"></script>
+	<script src="../js/bootstrap.js"></script>
+	<script src="../js/owl.carousel.js"></script>
+	<script src="../js/jquery.dlmenu.js"></script>
+	<script src="../js/jquery.sticky.js"></script>
+	<script src="../js/jquery.nice-select.min.js"></script>
+	<script src="../js/jquery.magnific-popup.js"></script>
+	<script src="../js/jquery.bxslider.min.js"></script>
+	<script src="../js/venobox.min.js"></script>
+	<script src="../js/smothscroll_part1.js"></script>
+	<script src="../js/smothscroll_part2.js"></script>
+	<script src="../js/plugin/rs_slider/jquery.themepunch.revolution.min.js"></script>
+	<script src="../js/plugin/rs_slider/jquery.themepunch.tools.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.addon.snow.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.actions.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.carousel.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.kenburn.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.layeranimation.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.migration.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.navigation.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.parallax.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.slideanims.min.js"></script>
+	<script src="../js/plugin/rs_slider/revolution.extension.video.min.js"></script>
+	<script src="../js/custom.js"></script>
+	<!--main js file end-->
+</body>
+
+
+<!-- Mirrored from www.webstrot.com/html/moviepro/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 16 Nov 2023 12:18:52 GMT -->
+</html>
