6d1e907c8b44717aad36cc1f3d89a1588723bff2
thang31798 <thang1233214q@gmail.com>
2023-11-17 15:51:52 +0700

    add code

---
+++ pdo/danhmuc.php
@@ -0,0 +1,26 @@
+<?php
+
+function insert_danhmuc($tenloai){
+    $sql = "INSERT INTO danhmuc(name) values('$tenloai')";
+    pdo_execute($sql);
+}
+
+function delete_danhmuc($id){
+    $sql = "DELETE FROM danhmuc where id=".$id;
+    pdo_execute($sql);
+}
+function loadall_danhmuc(){
+    $sql="SELECT * FROM danhmuc order by id desc";
+    $listdanhmuc=pdo_query($sql);
+    return $listdanhmuc;
+}
+function loadone_danhmuc($id){
+    $sql = "SELECT * FROM danhmuc where id=".$id;
+    $dm=pdo_query_one($sql);
+    return $dm;
+}
+function update_danhmuc($id,$tenloai){
+    $sql = "update danhmuc set name='".$tenloai."' where id=".$id;
+    pdo_execute($sql);
+}
+?>
